04eb061d16dc3cce53a37c373375d336be12e28d
ai-chemist <utk8244@gmail.com>
2025-08-14 13:35:26 +0900

    Start Project

---
+++ README.md
@@ -0,0 +1,14 @@
+## Python Crash Course - \[Alien Invasion with pygame] (12 to 14)
+
+---
+
+### Process
+
+|  날짜   |         내역          |        비고         |
+|:-----:|:-------------------:|:-----------------:|
+| 08/14 | chapter 12 (pygame) | main part of game |
+
+
+---
+
+*Based on the book by **"Eric Matthes"***
---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """게임 전체 관리"""
+
+    def __init__(self):
+        """게임 초기화 및 리소스 생성"""
+        pygame.init()
+
+        # 프레임 조절을 위한 clock
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((1600, 1200))
+        pygame.display.set_caption('Alien Invasion')
+
+        # 배경 색상 설정 - 기본값은 검은색 화면
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        """게임의 기능 실행"""
+        while True:
+            # 사용자 입력에 응답
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # 매 루프마다 화면 다시 출력
+            self.screen.fill(self.bg_color)
+
+            # 가장 최근의 화면 출력
+            pygame.display.flip()
+
+            # 프레임 전달 - 초당 60 프레임
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # 게임 인스턴스 생성 및 실행
+    ai = AlienInvasion()
+    ai.run_game()
