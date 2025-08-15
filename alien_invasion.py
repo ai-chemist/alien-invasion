7e34bb53d76a71ee553985970dbba2b2bc5fcb19
ai-chemist <utk8244@gmail.com>
2025-08-15 14:08:44 +0900

    run method refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,20 +30,27 @@ class AlienInvasion:
     def run_game(self):
         """게임의 기능 실행"""
         while True:
-            # 사용자 입력에 응답
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # 매 루프마다 화면 다시 출력
-            self.screen.fill(self.settings.bg_color)
-            self.space_ship.blitme()
-
-            # 가장 최근의 화면 출력
-            pygame.display.flip()
-
+            self._check_events()
+            self._update_screen()
             # 프레임 전달 - 초당 60 프레임
             self.clock.tick(60)
+
+    # _method() - 보조 메서드(클래스 내부에서만 사용하는 메서드) 를 나타내기 위해 _method_name() 방식으로 작성
+    def _check_events(self):
+        """사용자 입력에 응답 - run_game()에서 분리"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """화면 업데이트 메서드 - run_game()에서 분리"""
+        # 매 루프마다 화면 다시 출력
+        self.screen.fill(self.settings.bg_color)
+        self.space_ship.blitme()
+
+        # 가장 최근의 화면 출력
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # 게임 인스턴스 생성 및 실행
